fb769518099c6365de7ef3b69e075d620b9f06d7
zeba_ali <zeba@zeba-alis-MacBook-Pro.local>
2020-04-20 00:34:16 +0530

    initial commit

---
+++ src/main/java/com/basic/kafka/Basic_Kafka/BasicKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.basic.kafka.Basic_Kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basic/kafka/Basic_Kafka/BasicKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.kafka.Basic_Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
