39eb5cb53ad1bfc22b22cc7b38e2980d64ac1395
zeba_ali <zeba@zeba-alis-MacBook-Pro.local>
2020-04-19 22:03:16 +0530

    initial commit

---
+++ main/java/com/practice_kafka/PracticeKafka/PracticeKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.practice_kafka.PracticeKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeKafkaApplication.class, args);
+	}
+
+}
---
+++ test/java/com/practice_kafka/PracticeKafka/PracticeKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice_kafka.PracticeKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
